ac0467f45457046053e3d0ba68918eac964eb91e
BrenKenna <brend.k.bk@gmail.com>
2021-11-30 19:46:53 +0000

    addressed speed, S1-Q2 breaks :(

--- webpage/backup/run_queries.js
+++ webpage/backup/run_queries.js
@@ -10,127 +10,10 @@
 */
 
 /**
- * Read database
- */
- function readDatabase (dbPath) {
-	var l = dbPath.length,
-	arr = new Uint8Array(l);
-	for (var i=0; i<l; i++) arr[i] = dbPath.charCodeAt(i);
-	return arr;
-}
-
-/**
  * Function to manage running queries to the connected database
  * & and delivery the results as content to the TV-Div
  */
- function manageQueries(binArrayDB, query) {
-
-    // Initial needed variables & inform user of table loading
-    let con, counter, results, row, columns, out, colHead, col, data;
-    console.log(query);
-
-    // Create connection
-    con = new SQL.Database(binArrayDB);
-    binArrayDB = null;
-
-    // Reset table
-    $("#resultsBlock").html(`
-        <table id="resultsTable" class="table">
-            <thead id="colHead"></thead>
-            <tbody id="resultsBody"></tbody>
-        </table>
-    `);
-
-    // Run query: stmnt.free()
-    results = con.exec(query);
-    con.close();
-    columns = results[0]["columns"];
-
-    /**
-     * Serve the colHead content
-    */
-    colHead = `<tr><th scope="col">#</th>`;
-            
-    // Loop through columns
-    for(col of columns) {
-        colHead = `${colHead}<th scope="col">${col}</th>`;
-    }
-
-    // Close colHead
-    colHead = colHead + "</tr>";
-
-    // Set content and clear table body
-    $("#colHead").html(colHead);
-
-    // Handle pushing a count
-    counter = 1;
-    data = [];
-    if(results[0]["values"].length == 1 ) {
-
-        // Initalize count row
-        console.log("Pushing count");
-        row = results[0]["values"][0];
-        out = `<tr><th scope="row">${counter}</th>`;
-
-        // Add each column to the row
-        for (col of row){
-            out = out + `<td>${col}</td>`;
-        }
-
-        // Close row
-        out = out + `</tr>`;
-
-        // Add to the data object
-        data.push(out);
-        $("#resultsBody").html(data.toString());
-
-    } else {
-
-        // Otherwise the handle results as a table
-        console.log("Pushing rows");
-        for(row of results[0]["values"]){
-
-            // Initalize the row
-            out = `<tr><th scope="row">${counter}</th>`;
-
-            // Add each column to the row
-            for (col of row){
-                out = out + `<td>${col}</td>`;
-            }
-
-            // Close row
-            out = out + `</tr>`;
-            data.push(out);
-
-            // Handle placement: Not completely necessary given empty division but useful to know
-            if (data.length == 5 ) {
-                console.log(`Pushing next batch of table rows`);
-                if(counter == 5) {
-                    $("#resultsBody").html(data.toString());
-                }
-                else {
-                    $("#resultsBody").append(data.toString());
-                }
-                data = [];
-            }
-            counter++;
-        }
-    }
-
-    // Clean up
-    data = [];
-    results = null;
-    console.log(`Query complete with N rows = ${counter-1}`);
-}
-
-
-
-
-/**
- * Function to manage running queries to the connected database
- * & and delivery the results as content to the TV-Div
- */
-function manageQueries_old(query) {
+function manageQueries(query) {
 
     // Initial needed variables & inform user of table loading
     let xhttp, db, counter, results, row, columns, out, colHead, col, data;
@@ -185,7 +68,7 @@ function manageQueries_old(query) {
 
             // Serve results to tableDiv
             data = [];
-            for(row of results[0]["values"]){
+             for(row of results[0]["values"]){
                 
                 // Initalize the row
                 out = `<tr><th scope="row">${counter}</th>`;
@@ -224,11 +107,10 @@ function manageQueries_old(query) {
 
             // Clean up
             data = [];
-            results = null;
-            db = null;   
+            results = null;    
         }
     };
 }
 
 // Export the manage queries function
-export {manageQueries, manageQueries_old};
+export default manageQueries;
