38672f59464994a78fbb7e2c7e070abd20cb9e2c
BrenKenna <brend.k.bk@gmail.com>
2021-12-04 15:58:24 +0000

    archived old files and future plans

--- javascript/ajax/setup-db.sh
+++
@@ -1,8 +0,0 @@
-echo -e "create table testing(FirstName varchar(50), LastName varchar(250), Age int(3));" | sqlite3 testing.db
-echo -e "
-    insert into testing values ('John', 'Doe', 32);
-    insert into testing values ('Jane', 'Doe', 29);
-    insert into testing values ('Bill', 'Joe', 35);
-    insert into testing values ('Martha', 'Grand', 28);
-" | sqlite3 testing.db
-echo -e "select * from testing order by random();" | sqlite3 testing.db
